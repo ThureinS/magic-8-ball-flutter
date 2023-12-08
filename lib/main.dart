4149dd6ada4837ce74d1e1e9b152a3ab649017fd
ThureinS <94038134+ThureinS@users.noreply.github.com>
2023-12-08 23:24:27 +0630

    Complete maigic 8 ball flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold (
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: magicBall(),
+        ),
       ),
     );
+class magicBall extends StatefulWidget {
+  const magicBall({super.key});
+
+  @override
+  State<magicBall> createState() => _magicBallState();
+}
+
+class _magicBallState extends State<magicBall> {
+  int ballNum = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNum=Random().nextInt(5)+1;
+          });
+      }, child: Image(
+        image: AssetImage('images/ball$ballNum.png'),
+      ),
+      ),
+    );
+  }
+}
